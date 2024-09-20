afad4bd9a26f94cecc660341f5be3bb1585e1767
Fahim Mohammod Fardous <fahim.fardous@brainstation-23.com>
2024-09-20 14:33:26 +0600

    Add download progress

--- app/src/main/java/com/example/taskhiveapp/base/TaskHive.kt
+++ app/src/main/java/com/example/taskhiveapp/base/TaskHive.kt
@@ -1,11 +1,31 @@
 package com.example.taskhiveapp.base
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TaskHive : Application() {
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel(this)
     }
 }
+
+fun createNotificationChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = "File Upload"
+        val descriptionText = "Notifications for file upload progress"
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channel = NotificationChannel("upload_channel_id", name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
